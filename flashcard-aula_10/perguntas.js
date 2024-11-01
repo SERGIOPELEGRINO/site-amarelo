ba83b5e3340d7d4d428f7928a5e570bdc1795ef9
SERGIOPELEGRINO <161846267+SERGIOPELEGRINO@users.noreply.github.com>
2024-11-01 10:34:54 -0300

    Add files via upload

---
+++ flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é a linguagem JAVA?',
+    'é uma linguagem de programação popular orientada a objetos e multiplataforma.'
+)
+
+criaCartao(
+    'Geografia',
+    'O agronegocio representa quantos % do PIB do Paraná?',
+    'Representa 35% do PIB'
+)
+
+criaCartao(
+    'Programação',
+    'O que é a linguagem html?',
+    'é uma linguagem de marcação utilizada na construção de páginas na Web'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual é o maior time do Brasil?',
+    'Santos Futebol Clube!!!'
+)
